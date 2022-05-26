9479da528047655b6c1540acaef72b0d79ee6e25
ibktommy <atomquartz@gmail.com>
2022-05-26 11:52:14 +0100

    Making the canvas container wall thinner

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ Runner.run(Runner.create(), engine);
 
 // Creating Rectangular Walls
 const walls = [
-	Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-	Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-	Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
+	Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+	Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+	Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+	Bodies.rectangle(width, height / 2, 2, height, { isStatic: true }),
 ];
 
 World.add(world, walls);
